c8e59cf49f4679fe4f0bf8c1170736213e75d9d3
ravikiran.moola <MR244@sciit.inc>
2018-11-21 19:27:11 +0530

    um code

---
+++ src/main/webapp/js/controllers/headerController.js
@@ -0,0 +1,25 @@
+function headerController($scope, $cookies){
+	var res = $cookies.getObject('signInResult');
+	console.log(res); //Added for testing purpose.
+	
+	/* START: Uncomment to enable SA */
+	console.log(res);	
+	$scope.roleId = res.responsePayload.roleId;	 
+	 $scope.defaultPassword = res.responsePayload.isDefaultPasswordChanged;	
+	if($scope.secondaryAuthorization == 1){		
+		$scope.secondaryAuthorization = 1;
+	}else{		
+		$scope.secondaryAuthorization = res.responsePayload.isSecondaryAuthorizationSaved;
+	}	
+	
+	if($scope.secondaryAuthorization == 0){		
+		if($cookies.getObject('secondaryAuthorizationCompleted')){			
+			console.log("Entered after completing secondary Authorization");			
+			$scope.defaultPassword = 1;
+			$scope.secondaryAuthorization = 1;
+		}
+	}	
+	console.log("defaultPassword: "+$scope.defaultPassword);
+	console.log("secondaryAuthorization: "+$scope.secondaryAuthorization);
+	/* END: Uncomment to enable SA */	
+}
